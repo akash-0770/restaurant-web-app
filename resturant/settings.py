48eab4733c047e162cb98011aec34769f119c0f9
akash <akashghorpade2601@gmail.com>
2026-02-05 14:59:04 +0530

    Fix STATIC_ROOT typo for django collectstaticfiles

--- resturant/settings.py
+++ resturant/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR/"static")]
 
-STAIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = 'media/'
 
